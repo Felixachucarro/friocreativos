6f1e68ffe471ac768d9390dd0f52759e00d9c718
felix achucarro <felix@entornos.net>
2023-11-03 15:26:24 -0300

    changes

--- js/date.js
+++ js/date.js
@@ -1,31 +1,23 @@
-
-
-
 // Inicializa Firebase con tu configuración
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-app.js";
-    import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-analytics.js";
-    // TODO: Add SDKs for Firebase products that you want to use
-    // https://firebase.google.com/docs/web/setup#available-libraries
-  
-    // Your web app's Firebase configuration
-    // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-    const firebaseConfig = {
-      apiKey: "AIzaSyBVg9vvfDrnZRgiJDZiZ-apzRSjNxh3RBU",
-      authDomain: "frio-creativos.firebaseapp.com",
-      projectId: "frio-creativos",
-      storageBucket: "frio-creativos.appspot.com",
-      messagingSenderId: "615296536631",
-      appId: "1:615296536631:web:5d7ae00bf2635c78d4efb2",
-      measurementId: "G-E42XGEF7L3"
-    };
-  
-    // Initialize Firebase
-    const app = initializeApp(firebaseConfig);
-    const analytics = getAnalytics(app);
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-analytics.js";
 
+// Tu configuración de Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyBVg9vvfDrnZRgiJDZiZ-apzRSjNxh3RBU",
+  authDomain: "frio-creativos.firebaseapp.com",
+  projectId: "frio-creativos",
+  storageBucket: "frio-creativos.appspot.com",
+  messagingSenderId: "615296536631",
+  appId: "1:615296536631:web:5d7ae00bf2635c78d4efb2",
+  measurementId: "G-E42XGEF7L3"
+};
 
+// Inicializa Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
-firebase.initializeApp(firebaseConfig);
+// Accede a Firestore de Firebase
 const db = firebase.firestore();
 
 // Agrega un manejador de eventos cuando se envía el formulario
@@ -40,9 +32,6 @@ document.getElementById("contact-form").addEventListener("submit", function (e) 
     const instagram = document.getElementById("instagram").value;
     const investmentRange = document.getElementById("investment-range").value;
     const services = document.getElementById("services").value;
-
-    // Accede a Firestore de Firebase
-
 
     // Crea un objeto con los datos del formulario
     const formData = {
