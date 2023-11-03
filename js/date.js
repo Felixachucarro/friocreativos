30a07100956d1a83828cfafa0e7813e4fbcc48b7
felix achucarro <felix@entornos.net>
2023-11-03 15:17:10 -0300

    ks

--- js/date.js
+++ js/date.js
@@ -1,3 +1,6 @@
+import { initializeApp } from 'firebase/app';
+
+
 // Inicializa Firebase con tu configuración
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-app.js";
     import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-analytics.js";
